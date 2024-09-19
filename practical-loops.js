fea09ddf1c0b3ed4a8197e2a8cd369c00710aec4
sramirez124 <forwardemails124@gmail.com>
2024-09-19 14:28:21 -0500

    in class work added

--- practical-loops.js
+++ practical-loops.js
@@ -1,10 +1,26 @@
 // Part 1: Refactoring Old Code
-const csv = [
-["ID", "Name", "Occupation", "Age"]
-["42", "Bruce", "Knight", "41"]
-["57", "Bob", "Fry Cook", "19"]
-["63", "Blaine", "Quiz Master", "58"]
-["98", "Bill", "Doctor’s Assistant", "26"]
-]
+const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+
+const cells = [];
+let currentCell = 0;
 
 // Part 2: Expanding Functionality
+for (let i = 0; i < csv.length; i++) {
+    if(csv[i] === " , "){
+        currentCell++
+        continue;
+    }
+
+    if (csv[i] === " \n "){
+        cells = [];
+        currentCell = 1;
+        continue;
+    }
+
+    if (cells[currentCell]){
+    cells[currentCell] += csv[i];
+    } else {
+        cells[currentCell] = csv[i];
+    }
+}
+
