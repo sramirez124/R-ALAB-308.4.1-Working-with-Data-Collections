f6601869c6d0e4e2660b5aee0861bdee5f0bc8c3
Steven Ramirez <forwardemails124@gmail.com>
2024-09-18 21:59:47 -0500

    refactor the refactor

--- practical-loops.js
+++ practical-loops.js
@@ -1,8 +1,12 @@
 // Part 1: Refactoring Old Code
-let cell1 = ["ID", "Name", "Occupation", "Age"]
-let cell2 = ["42", "Bruce", "Knight", "41"]
-let cell3 = ["57", "Bob","Fry Cook", "19"];
-let cell4 = ["63", "Blaine", "Quiz Master", "58"];
-let cell5 = ["98", "Bill", "Doctor’s Assistant", "26"]
+const csv = 
+[
+["ID", "Name", "Occupation", "Age"]
+["42", "Bruce", "Knight", "41"]
+["57", "Bob","Fry Cook", "19"]
+["63", "Blaine", "Quiz Master", "58"]
+["98", "Bill", "Doctor’s Assistant", "26"]
+]
 
-// Part 2: 
+// Part 2: Expanding Functionality
+console.log("hello");
