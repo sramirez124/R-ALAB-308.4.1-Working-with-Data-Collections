6e89a35ff069fb8828a9c9e17ae27f699e13e1e7
sramirez124 <forwardemails124@gmail.com>
2024-09-19 14:34:13 -0500

    refactor of the refactor of the refactor done

--- practical-loops.js
+++ practical-loops.js
@@ -1,19 +1,20 @@
 // Part 1: Refactoring Old Code
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 
-const cells = [];
+let cells = [];
 let currentCell = 0;
 
 // Part 2: Expanding Functionality
 for (let i = 0; i < csv.length; i++) {
     if(csv[i] === " , "){
-        currentCell++
+        currentCell++;
         continue;
     }
 
+    // checking for a new row
     if (csv[i] === " \n "){
         cells = [];
-        currentCell = 1;
+        currentCell = 0;
         continue;
     }
 
@@ -21,6 +22,13 @@ for (let i = 0; i < csv.length; i++) {
     cells[currentCell] += csv[i];
     } else {
         cells[currentCell] = csv[i];
+    }
+
+    if(
+        (currentCell === 3 && csv[i + 1] === '\n') ||
+        i + 1 === csv.length
+    ){
+        console.log(cells.toString());
     }
 }
 
