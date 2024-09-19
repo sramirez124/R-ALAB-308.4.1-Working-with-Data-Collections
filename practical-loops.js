3f9af49f6eedd6e1b83773beed1568ea7dec2dc5
Steven Ramirez <forwardemails124@gmail.com>
2024-09-18 21:41:58 -0500

    CSV now is in 5 arrays

---
+++ practical-loops.js
@@ -0,0 +1,8 @@
+// Part 1: Refactoring Old Code
+let cell1 = ["ID", "Name", "Occupation", "Age"]
+let cell2 = ["42", "Bruce", "Knight", "41"]
+let cell3 = ["57", "Bob","Fry Cook", "19"];
+let cell4 = ["63", "Blaine", "Quiz Master", "58"];
+let cell5 = ["98", "Bill", "Doctor’s Assistant", "26"]
+
+// Part 2: 
