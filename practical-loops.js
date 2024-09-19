ea90b5c758256a4819b4c87afe532a8ce765e86e
Steven Ramirez <forwardemails124@gmail.com>
2024-09-18 22:06:49 -0500

    committing my work

--- practical-loops.js
+++ practical-loops.js
@@ -1,12 +1,10 @@
 // Part 1: Refactoring Old Code
-const csv = 
-[
+const csv = [
 ["ID", "Name", "Occupation", "Age"]
 ["42", "Bruce", "Knight", "41"]
-["57", "Bob","Fry Cook", "19"]
+["57", "Bob", "Fry Cook", "19"]
 ["63", "Blaine", "Quiz Master", "58"]
 ["98", "Bill", "Doctor’s Assistant", "26"]
 ]
 
 // Part 2: Expanding Functionality
-console.log("hello");
